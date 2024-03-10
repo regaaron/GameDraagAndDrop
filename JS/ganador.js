18e017eb63e4502b9f5df10db335b0f1ba4a5e15
OmarReyesRmz <reyesomar028@gmail.com>
2024-03-10 12:21:05 -0600

    Update scores

--- JS/ganador.js
+++ JS/ganador.js
@@ -4,14 +4,24 @@ if (users == null) users = [];
 var indice = localStorage.getItem("indice");
 indice = JSON.parse(indice);
 var tiempo = localStorage.getItem("segundos");
+var tiempo2 = localStorage.getItem("minutos");
 
 const user = JSON.parse(users[indice]);
+
+// Guardar el tiempo en el objeto de usuario
+user.tiempo = `${tiempo2}:${tiempo}`;
+
+// Guardar el usuario actualizado de nuevo en el arreglo de usuarios
+users[indice] = JSON.stringify(user);
+
+// Guardar los usuarios actualizados en el almacenamiento local
+localStorage.setItem("users", JSON.stringify(users));
 
 window.onload = function () {
     var audio = document.getElementById("audio-victoria");
     audio.play();
     document.getElementById("titulo-ganador").innerHTML = `Felicidades ${user.userName}`;
-    document.getElementById("score").innerHTML = `Tu tiempo fue de: ${tiempo}`;
+    document.getElementById("score").innerHTML = `Tu mejor tiempo fue de ${tiempo2}:${tiempo}. ¡Toca superarlo!`;
     const jsConfetti = new JSConfetti();
     jsConfetti.addConfetti({
       emojis: ["✨","🪙"],
