92478fb0922f6cc3d2324a29acf4f8740ec071aa
regaaron <aaron_lopez222@hotmail.com>
2024-03-10 02:56:38 -0600

    Agregando los botones de sonido

--- JS/botones.js
+++ JS/botones.js
@@ -1,29 +1,41 @@
-let banderaSonido = false;
+// let banderaSonido = false;
 
 function mute() {
     const miAudio = document.getElementById("audio");
     const logosMute = document.getElementsByClassName("volumen");
-    alert("bandera: " + banderaSonido);
 
-    if (!banderaSonido) {
-        miAudio.play();
-        miAudio.loop = true;
-        banderaSonido = true;
-
-        for (const logo of logosMute) {
+    for (const logo of logosMute) {
+        if (miAudio.muted) {
             logo.classList.add("fa-volume-high");
             logo.classList.remove("fa-volume-xmark");
+        } else {
+            logo.classList.remove("fa-volume-high");
+            logo.classList.add("fa-volume-xmark");
         }
-    } else {
-        for (const logo of logosMute) {
-            if (miAudio.muted) {
-                logo.classList.add("fa-volume-high");
-                logo.classList.remove("fa-volume-xmark");
-            } else {
-                logo.classList.remove("fa-volume-high");
-                logo.classList.add("fa-volume-xmark");
-            }
-        }
-        miAudio.muted = !miAudio.muted;
     }
+    miAudio.muted = !miAudio.muted;
+
+    // alert("bandera: " + banderaSonido);
+
+    // if (!banderaSonido) {
+    //     miAudio.play();
+    //     miAudio.loop = true;
+    //     banderaSonido = true;
+
+    //     for (const logo of logosMute) {
+    //         logo.classList.add("fa-volume-high");
+    //         logo.classList.remove("fa-volume-xmark");
+    //     }
+    // } else {
+    //     for (const logo of logosMute) {
+    //         if (miAudio.muted) {
+    //             logo.classList.add("fa-volume-high");
+    //             logo.classList.remove("fa-volume-xmark");
+    //         } else {
+    //             logo.classList.remove("fa-volume-high");
+    //             logo.classList.add("fa-volume-xmark");
+    //         }
+    //     }
+    //     miAudio.muted = !miAudio.muted;
+    // }
 }
