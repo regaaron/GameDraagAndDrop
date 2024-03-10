2ed5b9e451159dd41965186a1e83d1fe5f3df31f
regaaron <aaron_lopez222@hotmail.com>
2024-03-10 11:33:00 -0600

    favicon y cambios alos estilos

--- JS/ganador.js
+++ JS/ganador.js
@@ -3,17 +3,18 @@ users = JSON.parse(users);
 if (users == null) users = [];
 var indice = localStorage.getItem("indice");
 indice = JSON.parse(indice);
+var tiempo = localStorage.getItem("segundos");
 
 const user = JSON.parse(users[indice]);
 
 window.onload = function () {
     var audio = document.getElementById("audio-victoria");
     audio.play();
-    document.getElementById("titulo-ganador").innerHTML = `Felicidades ${user.userName}`
-
+    document.getElementById("titulo-ganador").innerHTML = `Felicidades ${user.userName}`;
+    document.getElementById("score").innerHTML = `Tu tiempo fue de: ${tiempo}`;
     const jsConfetti = new JSConfetti();
     jsConfetti.addConfetti({
-      emojis: ["✨"],
+      emojis: ["✨","🪙"],
       confettiRadius: 6,
       confettiNumber: 60,
     });
